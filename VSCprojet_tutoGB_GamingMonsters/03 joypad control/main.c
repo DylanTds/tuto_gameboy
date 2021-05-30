d55d97356a67db99d21151b8afcaf1d968b8ebf4
Dylan Tavares de Sá <dylan.tavares.de.sa@gmail.com>
2021-05-30 20:51:15 +0200

    Fin video 3

--- VSCprojet_tutoGB_GamingMonsters/03 joypad control/main.c
+++ VSCprojet_tutoGB_GamingMonsters/03 joypad control/main.c
@@ -1,26 +1,33 @@
 #include <gb/gb.h>
 #include <stdio.h>
-#include "HeroSprite.c" // importation des sprites de "Hero"
+#include "HeroSprite.c"
 
 void main(){
-    UINT8 currTileIndex = 0;         // variable pour la valeur de la tile de "Hero" que l'on veut assigner au sprite "0"
+    UINT8 currTileIndex = 0;
 
     set_sprite_data(0, 2, Hero);
     set_sprite_tile(0, 0);
-    move_sprite(0, 0, 78);
+    move_sprite(0, 84, 80);
     SHOW_SPRITES;
 
-    while(1){                       // boucle infinie
-        if(currTileIndex==0){       // alterne automatiquement entre les deux valeurs pour la tile
-            currTileIndex = 1;
-        }else{
-            currTileIndex = 0;
+    while(1){
+        switch(joypad()){               // joypad() est fonction qui recupere l'input du joueur et retourne une constante (    J_A J_B J_START J_SELECT)
+            case J_UP:
+                scroll_sprite(0, 0, -8);
+                break;
+            case J_DOWN:
+                scroll_sprite(0, 0, 8);
+                break;
+            case J_LEFT:
+                scroll_sprite(0, -8, 0);
+                break;
+            case J_RIGHT:
+                scroll_sprite(0, 8, 0);
+                break;
+            
         }
+        delay(100);
 
-        set_sprite_tile(0, currTileIndex); // assigne la nouvelle valeur de tile au sprite "0"
 
-        delay(500);                 // attends 500 milisecondes avant de passer a la prochaine commande
-
-        scroll_sprite(0, 10, 0);    // decale le sprite "0" de x+10.
     }
 }
