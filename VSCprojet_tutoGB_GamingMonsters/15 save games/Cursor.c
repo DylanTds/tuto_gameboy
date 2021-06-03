ea82b9338248bb1971012361165527eaad92d206
Dylan Tavares de Sá <dylan.tavares.de.sa@gmail.com>
2021-06-03 18:10:18 +0200

    debut video 5

---
+++ VSCprojet_tutoGB_GamingMonsters/15 save games/Cursor.c
@@ -0,0 +1,8 @@
+#include <gb/gb.h>
+
+struct Cursor{
+    UINT8 x;
+    UINT8 y;
+    UINT8 col;
+    UINT8 row;
+};
---
+++ VSCprojet_tutoGB_GamingMonsters/15 save games/savestate.c
@@ -0,0 +1,3 @@
+#include <gb/gb.h>
+
+UINT8 txt[11];          // txt saisi par l'utilisateur
